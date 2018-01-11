f0887f205abed8cf3fb435b81eeb0fdfdb56cc6a
kutylamaciej <maciejo117@gmail.com>
2018-01-11 13:12:27 +0100

    zmieniłam Fibo.c

--- Fibo.c
+++ Fibo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int i, n, t1 = 0, t2 = 1, nextTerm;
+    int i, n, t1 = 0, t2 = 1, nextTerm, zmienna;
 
     printf("Enter the number of terms: ");
     scanf("%d", &n);
